6754d8012e7f91b1a4538b7b95fdb091c7672654
Jan Christoph Thiele <63301724+thielec@users.noreply.github.com>
2024-06-28 12:13:46 +0100

    Reduced default number of epoches

--- train.py
+++ train.py
@@ -76,8 +76,8 @@ def main():
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=9, metavar='N',
-                        help='number of epochs to train (default: 9)')
+    parser.add_argument('--epochs', type=int, default=4, metavar='N',
+                        help='number of epochs to train (default: 4)')
     parser.add_argument('--lr', type=float, default=1.0, metavar='LR',
                         help='learning rate (default: 1.0)')
     parser.add_argument('--gamma', type=float, default=0.7, metavar='M',
